5a4e14de4e461f27e4e76507a11cc4a5ef2bda61
Kyle Knutson <kyle.a.knutson@gmail.com>
2020-04-06 21:15:08 -0500

    ReduxThunk

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
     Form, FormGroup, Input, Label, Col, Row  } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
    
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
@@ -51,6 +52,26 @@ function RenderComments({comments, addComment, campsiteId}) {
 
 }
 function CampsiteInfo(props) {
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
     if (props.campsite) {
         return (
             <div className="container">
@@ -108,7 +129,7 @@ class CommentForm extends React.Component {
         this.toggleModal();
         this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
     }
-    
+
     toggleModal() {
         this.setState({
           isModalOpen: !this.state.isModalOpen
